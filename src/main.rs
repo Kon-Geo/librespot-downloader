9f26275b33d1562aaf1a5420e43827953ad977d1
KonGeo <kongeo2008@gmail.com>
2026-07-16 20:50:11 +0300

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashMap, fs::{File, create_dir_all}, io::{self, Seek, SeekFrom, copy}, path::PathBuf, process::exit
+    collections::HashMap, io::{self, Seek, SeekFrom, copy}, path::{Path, PathBuf}, fs::{File, create_dir_all}, process::exit, sync::Arc
 };
 use librespot::{
     audio::{AudioDecrypt, AudioFile}, core::{
@@ -18,6 +18,7 @@ use lofty::{
 use http::{HeaderValue, Method, Request, header::ACCEPT};
 use bytes::Bytes;
 use sanitize_filename::sanitize;
+use tokio::sync::RwLock;
 
 const CACHE: &str = ".cache";
 const CACHE_FILES: &str = ".cache/files";
@@ -94,20 +95,25 @@ fn format_data_rate(format: AudioFileFormat) -> usize {
     data_rate.ceil() as usize
 }
 
+pub struct Cover {
+    data: Vec<u8>,
+    mime: MimeType,
+}
+
 pub struct Downloader {
-    pub session: Session,
-    album_cover_cache: HashMap<String, (Vec<u8>, MimeType)>,
+    pub session: Arc<Session>,
+    pub album_cover_cache: Arc<RwLock<HashMap<String, Arc<Cover>>>>,
 }
 
 impl Downloader {
-    pub fn new(session: Session) -> Self {
+    pub fn new(session: Arc<Session>) -> Self {
         Self {
             session,
-            album_cover_cache: HashMap::new(),
+            album_cover_cache: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
-    pub async fn download_from_stdin(&mut self, directory: &str) -> Result<(), Error> {
+    pub async fn download_from_stdin(&mut self, directory: &Path) -> Result<(), Error> {
         info!("Enter a Spotify Track/Album/Playlist URL: ");
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
@@ -118,7 +124,7 @@ impl Downloader {
         self.download_by_url(input, directory).await
     }
 
-    pub async fn download_by_url(&mut self, input: &str, directory: &str) -> Result<(), Error> {
+    pub async fn download_by_url(&mut self, input: &str, directory: &Path) -> Result<(), Error> {
         let url = input.trim();
         let path = url
             .split("open.spotify.com/")
@@ -144,13 +150,13 @@ impl Downloader {
         }
     }
 
-    pub async fn download_playlist_by_id(&mut self, id: SpotifyId, directory: &str) -> Result<(), Error> {
+    pub async fn download_playlist_by_id(&mut self, id: SpotifyId, directory: &Path) -> Result<(), Error> {
         let uri = SpotifyUri::Playlist { user: Some("".to_string()), id };
         let playlist = Playlist::get(&self.session, &uri).await?;
         self.download_playlist(playlist, directory).await
     }
 
-    pub async fn download_playlist(&mut self, album: Playlist, directory: &str) -> Result<(), Error> {
+    pub async fn download_playlist(&mut self, album: Playlist, directory: &Path) -> Result<(), Error> {
         let mut dirpath = PathBuf::from(directory);
         dirpath.push(&album.name());
         info!("<{}> Playlist \"{}\" will be saved at {:?}", album.id.to_id()?, album.name(), dirpath);
@@ -158,48 +164,45 @@ impl Downloader {
         Ok(())
     }
 
-    pub async fn download_artist_by_id(&mut self, id: SpotifyId, directory: &str) -> Result<(), Error> {
+    pub async fn download_artist_by_id(&mut self, id: SpotifyId, directory: &Path) -> Result<(), Error> {
         let uri = SpotifyUri::Artist { id };
         let artist = Artist::get(&self.session, &uri).await?;
         self.download_artist(artist, directory).await
     }
 
-    pub async fn download_artist(&mut self, artist: Artist, directory: &str) -> Result<(), Error> {
+    pub async fn download_artist(&mut self, artist: Artist, directory: &Path) -> Result<(), Error> {
         let mut dirpath = PathBuf::from(directory);
         dirpath.push(&artist.name);
         info!("<{}> Artist \"{}\" will be saved at {:?}", artist.id.to_id()?, artist.name, dirpath);
-        self.download_albums(artist.albums, directory, false).await?;
-        self.download_albums(artist.singles, directory, false).await?;
+        self.download_albums(artist.albums, &dirpath, false).await?;
+        self.download_albums(artist.singles, &dirpath, false).await?;
         Ok(())
     }
 
-    pub async fn download_album_by_id(&mut self, id: SpotifyId, directory: &str) -> Result<(), Error> {
+    pub async fn download_album_by_id(&mut self, id: SpotifyId, directory: &Path) -> Result<(), Error> {
         self.download_album_by_uri(&SpotifyUri::Album { id }, directory, false).await
     }
 
-    pub async fn download_album_by_uri(&mut self, uri: &SpotifyUri, directory: &str, dir_is_final: bool) -> Result<(), Error> {
+    pub async fn download_album_by_uri(&mut self, uri: &SpotifyUri, directory: &Path, dir_is_final: bool) -> Result<(), Error> {
         let album = Album::get(&self.session, uri).await?;
         self.download_album(album, &directory, dir_is_final).await
     }
 
-    pub async fn download_albums(&mut self, albums: AlbumGroups, directory: &str, merge: bool) -> Result<(), Error> {
+    pub async fn download_albums(&mut self, albums: AlbumGroups, directory: &Path, merge: bool) -> Result<(), Error> {
         for uri in albums.current_releases() {
             self.download_album_by_uri(uri, directory, merge).await?;
         };
         Ok(())
     }
 
-    pub async fn download_album(&mut self, album: Album, directory: &str, dir_is_final: bool) -> Result<(), Error> {
-        let mut dirpath = PathBuf::from(directory);
-        if !dir_is_final {
-            dirpath.push(&album.name);
-        }
+    pub async fn download_album(&mut self, album: Album, directory: &Path, dir_is_final: bool) -> Result<(), Error> {
+        let dirpath = if dir_is_final { PathBuf::from(directory) } else { directory.join(&album.name) };
         info!("<{}> Album \"{}\" will be saved at {:?}", album.id.to_id()?, album.name, dirpath);
         self.download_tracks(album.tracks(), &dirpath).await?;
         Ok(())
     }
 
-    async fn download_tracks(&mut self, tracks: impl Iterator<Item = &SpotifyUri>, dirpath: &PathBuf) -> Result<(), Error> {
+    async fn download_tracks(&mut self, tracks: impl Iterator<Item = &SpotifyUri>, dirpath: &Path) -> Result<(), Error> {
         for track_uri in tracks {
             let track = Track::get(&self.session, track_uri).await?;
             if let Err(e) = self.download_track(&track, &dirpath).await {
@@ -209,25 +212,26 @@ impl Downloader {
         Ok(())
     }
 
-    pub async fn download_track_by_id(&mut self, id: SpotifyId, directory: &str) -> Result<(), Error> {
+    pub async fn download_track_by_id(&mut self, id: SpotifyId, directory: &Path) -> Result<(), Error> {
         let uri = SpotifyUri::Track { id };
         let track = Track::get(&self.session, &uri).await?;
         let dirpath = PathBuf::from(directory);
         self.download_track(&track, &dirpath).await
     }
 
-    pub async fn download_track(&mut self, track: &Track, dirpath: &PathBuf) -> Result<(), Error> {
+    pub async fn download_track(&mut self, track: &Track, dirpath: &Path) -> Result<(), Error> {
         info!("<{}> Downloading Track #{}: \"{}\"", track.id.to_id()?, track.number, track.name);
         let track_id = match track.id {
             SpotifyUri::Track { id } => id,
             _ => return Ok(()),
         };
 
-        let fids = track.files.iter()
-            .map(|f| format!("{:?}", f.0))
+        let fids = track.files
+            .keys()
+            .map(|f| format!("{:?}", f))
             .collect::<Vec<_>>()
-            .join(", ")
-        ;
+            .join(", ");
+
         debug!("<{}> Has formats: {}", track.id.to_id().is_ok(), fids);
 
         let (format, file_id) = FORMAT_PREFERENCE.iter()
@@ -239,6 +243,7 @@ impl Downloader {
         let file_extension = get_extension_from_format(format);
         let filename = format!("{} - {} ({}).{}", artists_string, track.name, track.id.to_id()?, file_extension);
         let filename = sanitize(filename);
+        let filename = filename.chars().take(200).collect::<String>();
         let filepath = dirpath.join(filename);
         if filepath.exists() {
             warn!("<{}> Skipping Track #{}: \"{}\" (File Already Exists)", track.id.to_id()?, track.number, track.name);
@@ -252,7 +257,7 @@ impl Downloader {
         let offset = if AudioFiles::is_ogg_vorbis(format) { SPOTIFY_OGG_HEADER_END } else { 0 };
         decrypted_file.seek(SeekFrom::Start(offset))?;
 
-        create_dir_all(&dirpath)?;
+        create_dir_all(dirpath)?;
         let mut outfile = File::create(&filepath)?;
         copy(&mut decrypted_file, &mut outfile)?;
         info!("<{}> Saved Track #{}: \"{}\" to {:?}", track.id.to_id()?, track.number, track.name, filepath);
@@ -261,11 +266,11 @@ impl Downloader {
             "ogg" | "flac" => TagType::VorbisComments,
             _ => TagType::Id3v2,
         };
-        self.apply_tag(track, tag_type, filepath, artists_string).await?;
+        self.apply_tag(track, tag_type, &filepath, artists_string).await?;
         Ok(())
     }
 
-    async fn apply_tag(&mut self, track: &Track, tag_type: TagType, filepath: PathBuf, artists_string: String) -> Result<(), Error> {
+    async fn apply_tag(&mut self, track: &Track, tag_type: TagType, filepath: &Path, artists_string: String) -> Result<(), Error> {
         let mut tag = Tag::new(tag_type);
         tag.insert(TagItem::new(ItemKey::TrackTitle, ItemValue::Text(track.name.clone())));
         tag.insert(TagItem::new(ItemKey::AlbumTitle, ItemValue::Text(track.album.name.clone())));
@@ -273,8 +278,8 @@ impl Downloader {
         tag.insert(TagItem::new(ItemKey::TrackNumber, ItemValue::Text(track.number.to_string())));
         tag.insert(TagItem::new(ItemKey::Isrc, ItemValue::Text(track.id.to_uri()?)));
 
-        let (cover_data, mime_type) = self.get_cover(track).await?;
-        let picture = Picture::new_unchecked(PictureType::CoverFront, Some(mime_type.clone()), Some("cover".to_string()), cover_data.clone());
+        let cover = self.get_cover(track).await?;
+        let picture = Picture::new_unchecked(PictureType::CoverFront, Some(cover.mime.clone()), Some("cover".to_string()), cover.data.clone());
         tag.push_picture(picture);
 
         if let Err(e) = tag.save_to_path(&filepath, WriteOptions::default()) {
@@ -286,7 +291,7 @@ impl Downloader {
         Ok(())
     }
 
-    async fn get_cover(&mut self, track: &Track) -> Result<(&Vec<u8>, &MimeType), Error> {
+    async fn get_cover(&mut self, track: &Track) -> Result<Arc<Cover>, Error> {
         fn size_rank(size: image::ImageSize) -> i32 {
             match size {
                 image::ImageSize::DEFAULT => 0,
@@ -295,13 +300,20 @@ impl Downloader {
                 image::ImageSize::XLARGE => 3,
             }
         }
-        let cover = track.album.covers.iter().max_by_key(|cover| size_rank(cover.size)).unwrap();
+        let cover = track.album.covers
+            .iter()
+            .max_by_key(|c| size_rank(c.size))
+            .ok_or_else(|| Error::failed_precondition("Album has no cover"))?;
         let cover_id = cover.id.to_string();
-        if !self.album_cover_cache.contains_key(&cover_id) {
-            self.download_cover(&cover_id).await?;
+        {
+            let cache = self.album_cover_cache.read().await;
+            if let Some(cover) = cache.get(&cover_id) {
+                return Ok(Arc::clone(cover));
+            }
         }
-        let (cover_data, mime_type) = self.album_cover_cache.get(&cover_id).unwrap();
-        Ok((cover_data, mime_type))
+        self.download_cover(&cover_id).await?;
+        let cache = self.album_cover_cache.read().await;
+        Ok(Arc::clone(cache.get(&cover_id).unwrap()))
     }   
 
     async fn download_cover(&mut self, id: &String) -> Result<(), Error> {
@@ -311,11 +323,14 @@ impl Downloader {
             .header(ACCEPT, HeaderValue::from_static("image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8"))
             .body(Bytes::new())?;
         let body = self.session.http_client().request_body(request).await?;
-        let cover_data = body.to_vec();
-        let mime_type = infer::get(&cover_data)
-            .map(|t| MimeType::from_str(t.mime_type()))
-            .unwrap_or(MimeType::Jpeg);
-        self.album_cover_cache.insert(id.clone(), (cover_data, mime_type));
+        let cover = Arc::new(Cover {
+            data: body.to_vec(),
+            mime: infer::get(&body)
+                .map(|t| MimeType::from_str(t.mime_type()))
+                .unwrap_or(MimeType::Jpeg),
+        });
+        let mut cache = self.album_cover_cache.write().await;
+        cache.insert(id.to_owned(), cover);
         Ok(())
     }
 }
@@ -351,11 +366,11 @@ async fn main() -> Result<(), Error> {
         exit(1);
     }
     
-    let mut downloader = Downloader::new(session);
+    let mut downloader = Downloader::new(Arc::new(session));
 
-    loop {
-        downloader.download_from_stdin("downloads").await?;
-    }
+    let base_path = "downloads";
+    let directory: PathBuf = PathBuf::from(base_path);
+    while downloader.download_from_stdin(&directory).await.is_ok() {}
 
-    // Ok(())
+    Ok(())
 }
